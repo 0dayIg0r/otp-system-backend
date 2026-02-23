8941335d16c680840063ae4ee31c405aa9343db7
0dayig0r <igormedeiros.f@gmail.com>
2026-02-23 11:16:58 -0300

    remove zod attribute

--- src/schemas/auth-signin.ts
+++ src/schemas/auth-signin.ts
@@ -2,5 +2,4 @@ import { z } from "zod";
 
 export const authSignInSchema = z.object({
   email: z.string({message: "E-mail é obrigatório"}).trim().pipe(z.email({ error: "E-mail inválido" })),
-  password: z.string().min(6, { error: "Mínimo 6 caracteres" }),
 });
