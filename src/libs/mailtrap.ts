27f42502da96cb4f4cd9509297b93fa15e71b9bb
0dayig0r <igormedeiros.f@gmail.com>
2026-02-23 12:09:08 -0300

    fix token and sandbox  declaration

--- src/libs/mailtrap.ts
+++ src/libs/mailtrap.ts
@@ -1,8 +1,10 @@
 import { MailtrapClient } from "mailtrap"
+import "dotenv/config"
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
   const mailtrap = new MailtrapClient({
-    token: (process.env.MAILTRAP_API_KEY as string) || "",
+    token: process.env.MAILTRAP_API_KEY as string,
+    sandbox: true,
     testInboxId: 2989997,
   })
 
