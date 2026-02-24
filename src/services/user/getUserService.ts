fa9b8466da85b646b452613c9b2e1d15350a8ebc
0dayig0r <igormedeiros.f@gmail.com>
2026-02-24 13:19:50 -0300

    fix mvc

--- src/services/user/getUserService.ts
+++ src/services/user/getUserService.ts
@@ -1,5 +1,17 @@
 import { prisma } from "../../libs/prisma"
 
+export const getUserByEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  return user
+}
+
+
+
 export const getUserById = async (id: number) => {
   const user = await prisma.user.findUnique({
     where: {
